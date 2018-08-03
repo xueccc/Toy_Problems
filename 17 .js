339ce5db8bb885827dec4d6682a00d077cc485eb
Xue Chen <xueqingc@gmail.com>
2018-08-02 20:40:11 -0700

    toy problem

---
+++ 17 .js
@@ -0,0 +1,20 @@
+//Given a string containing digits from 2-9 inclusive, return all possible letter combinations that the number could represent.
+
+//A mapping of digit to letters (just like on the telephone buttons) is given below. Note that 1 does not map to any letters.
+
+//Input: "23", string containing digits
+// Output: ["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"]. array of combos
+//c: n/a
+//e: n/a
+
+'2': ['a', 'b', 'c']
+'3': ['d', 'e', 'f']
+
+//create phoneKeys
+//create combos
+//recursive function - index2, index3
+  //create lengthcounter
+  //base case:  when counter = input lenght
+    //add the combo to combos storage
+  //iterate throught the first array
+  //iterate the remaining arrays
